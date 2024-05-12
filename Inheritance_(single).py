619b546d99b3ccfb55c692fb5899b3f9f2088a97
Anubhav Yadav <ay6666@srmist.edu.in>
2024-05-12 21:13:09 +0530

    Inheritance Single

--- Inheritance_(single).py
+++ Inheritance_(single).py
@@ -1,22 +1,27 @@
 ############## Single Inhitance ##############
+
+# Parent Class
 class Car:
-    # parent class
+    # method 1 of parent class
     @staticmethod
     def start():
         print('Car is started')
     
-    # child class
+    # method 2 of parent class
     @staticmethod
     def stop():
         print('Car is stopped')
     
+# Child Class
 class Tesla(Car):
 
     def __init__(self, model):
         self.model = model
 
+# Creating object of child class
 car1 = Tesla('Model S')
 
+# Accessing attributes and methods of parent class
 print(car1.model)
 print(car1.start())
 
